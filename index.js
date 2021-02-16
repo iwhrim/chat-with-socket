023cc7771035eda869ebb2766741af682e7503b2
Gustavo Teixeira <iwhrim@gmail.com>
2021-02-16 10:40:24 -0300

    broadcasting messages

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const socketIO = require('socket.io');
 
 const app = express();
-
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, ()=> {
-    console.log('Running')
-})
+const server = app.listen(3000, ()=> {
+    console.log('Running');
+});
+
+const io = socketIO(server);
+
+const messages = [];
+
+io.on('connection', (socket)=> {
+    console.log('new connection');
+    socket.emit('update_messages', messages);
+
+    socket.on('new_message', (data)=> {
+        messages.push(data.msg);
+        io.emit('update_messages', messages);
+    });
+
+});
